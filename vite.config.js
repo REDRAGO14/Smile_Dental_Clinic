f3a4c033e316e991339a5401d097325c484fcd42
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-02-24 12:01:40 -0800

    fix minor error

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,3 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
-module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        primary: "#2D83E8", // your custom color
-      },
-    },
-  },
-  plugins: [],
-}
